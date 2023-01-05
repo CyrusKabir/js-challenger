f9501664fd2f69bae27821826fa0b2a89536a8ad
Cyruskabir <ariansepahi@yahoo.com>
2023-01-05 22:39:13 +0330

    Fix: add try/catch block

--- readme-automate.js
+++ readme-automate.js
@@ -2,17 +2,21 @@ const fs = require("fs");
 const solutions_obj = require("./solutions/all");
 const { processInject } = require("./src/inject");
 const readMeFilePath = "./README.md";
-fs.readFile(readMeFilePath, "utf-8", (err, readMeFileData_str) => {
-    if (err) {
-        console.log(err);
-        return err;
-    }
-    let readMeFileLines_arr = readMeFileData_str.split(/\r?\n/);
-    let result_str = processInject(solutions_obj, readMeFileLines_arr);
-    fs.writeFile(readMeFilePath, result_str, (err) => {
-        if (err) {
+
+try {
+    fs.readFile(readMeFilePath, "utf-8", (err, readMeFileData_str) => {
+        if(err) throw new Error(err)
+        let readMeFileLines_arr = readMeFileData_str.split(/\r?\n/);
+        let result_str = processInject(solutions_obj, readMeFileLines_arr);
+        try {
+            fs.writeFile(readMeFilePath, result_str, (err) => {
+                if(err) throw new Error(err)
+            });
+        } catch(err) {
             console.log(err);
-            return err
         }
-    });
-})
+        
+    })
+} catch (err) {
+    console.log(err);
+}
